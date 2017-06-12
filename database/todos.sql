42b1a209fd34f7d2c130932335c77ca6bdc734a7
giulianobaumann <giulianobaumann@googlemail.com>
2017-06-12 10:31:42 +0200

    SQL command for alter serial primary key
    Please run the sql command in the following file:
    
    database\todos.sql
    
    Run this file in the SQL command window of the NASAWIND database in pgadmin

---
+++ database/todos.sql
@@ -0,0 +1,7 @@
+-----------------------------------------------------------------------------------
+
+--Run the following command in the sql window of postgres of the NASAWIND database
+
+ALTER TABLE trees_latlon ADD COLUMN t_id SERIAL PRIMARY KEY;
+
+-----------------------------------------------------------------------------------
