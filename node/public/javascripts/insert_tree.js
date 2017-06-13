18200e9c4c887c316c4301674483f29620ea87e8
Cholgrrr <patrick.wuerstle@gmx.de>
2017-06-13 20:56:33 +0200

    Merge branch 'master' of https://github.com/Cholgrrr/nasaproject

---
+++ node/public/javascripts/insert_tree.js
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+Input: no parameter is required
+Output: json object with the following info of the new inserted point:
+		- koordinates latlon
+		- treetype
+		- tree age
+		- tree diameter
+
+This function reads the value of the entered tree, combine it in a json object 
+and post it to the server/database
+
+******************************************************************************/
+
+function insert_tree() {
+
+	try {
+		
+		let in_tree = {
+					   lat : 50.1586236986, 
+					   lon : 8.6274, 
+					   treetype : 'Ahorn', 
+					   age : 50, 
+					   diameter : 4
+					   }
+
+		console.log(in_tree);
+		
+		$.ajax({
+            type: "POST",
+            url: '/insertTree',
+            data: in_tree,
+        }).done(function() {});
+	
+	}
+	catch(err) {
+		console.log('delivering of the inserted tree data to the server failed!');
+	}
+	
+}
+
+
+
