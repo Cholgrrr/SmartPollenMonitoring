e3e836eccd53bf4303645a3d26ee0a34fd6fe022
Thunyathep Santhanavanich <thunyathep.s@outlook.com>
2017-07-01 02:18:10 +0200

    add button to show Stat again
    add button to show Stat again

--- node/public/javascripts/statistic.js
+++ node/public/javascripts/statistic.js
@@ -241,12 +241,18 @@ var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("showStat");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function () {
+    modal.style.display = "block";
+    d3.selectAll("svg > *").remove();
+    TreeStat();
+}
+btn2.onclick = function () {
     modal.style.display = "block";
     d3.selectAll("svg > *").remove();
     TreeStat();
