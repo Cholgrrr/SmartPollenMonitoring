334a22daf4e3294165378cb700b188af96fb51f9
Thunyathep Santhanavanich <thunyathep.s@outlook.com>
2017-07-03 18:13:00 +0200

    Presentation powerpoint
    Presentation powerpoint

--- node/server.js
+++ node/server.js
@@ -24,12 +24,12 @@ const pgp = require('pg-promise')({
 });
 
 const connection = {
-  user: 'nasaworldwind', 														// name of the user account
+  user: 'postgres', 														// name of the user account
   database: 'NASAWIND', 														// name of the database
   password: 'postgres', 														// env var: PGPASSWORD 
-  host: 'postgres-nasawind.cuefszt0dp4m.us-east-2.rds.amazonaws.com', 			// Server hosting the postgres database 
+  host: 'localhost', 			// Server hosting the postgres database 
   port: 5432, 																	// env var: PGPORT 
-  max: 40, 																		// max number of clients in the pool
+  max: 10, 																		// max number of clients in the pool
   idleTimeoutMillis: 30000 														// how long a client is allowed to remain idle before being closed
 }
 
