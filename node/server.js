d841b9036aefe7b68d8f75067b6cf33c21bb6ad4
giulianobaumann <giulianobaumann@googlemail.com>
2017-07-04 12:06:31 +0200

    google cloud

--- node/server.js
+++ node/server.js
@@ -24,10 +24,10 @@ const pgp = require('pg-promise')({
 });
 
 const connection = {
-  user: 'nasaworldwind', 														// name of the user account
+  user: 'postgres', 																// name of the user account
   database: 'NASAWIND', 														// name of the database
   password: 'postgres', 														// env var: PGPASSWORD 
-  host: 'postgres-nasawind.cuefszt0dp4m.us-east-2.rds.amazonaws.com', 			// Server hosting the postgres database 
+  host: '35.187.21.114', 			// Server hosting the postgres database 
   port: 5432, 																	// env var: PGPORT 
   max: 40, 																		// max number of clients in the pool
   idleTimeoutMillis: 30000 														// how long a client is allowed to remain idle before being closed
