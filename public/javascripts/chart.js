2697d8207f125235fd700f3740c14e42044b5819
Thunyathep Santhanavanich <thunyathep.s@outlook.com>
2018-07-14 15:42:32 +0200

    Skeleton for chart

---
+++ public/javascripts/chart.js
@@ -0,0 +1,56 @@
+
+var chartdata = [
+    { name: 'Apple', y: 600 },
+    { name: 'Cherry', y: 300 },
+    { name: 'Buch', y: 100 }
+];
+
+Highcharts.setOptions({
+    colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
+        return {
+            radialGradient: {
+                cx: 0.5,
+                cy: 0.3,
+                r: 0.7
+            },
+            stops: [
+                [0, color],
+                [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+            ]
+        };
+    })
+});
+
+// Build the chart
+Highcharts.chart('chartcontainer', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Tree Statistic (on Current Window)'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>({point.y} trees)'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                },
+                connectorColor: 'silver'
+            }
+        }
+    },
+    series: [{
+        name: 'Tree',
+        data: chartdata
+    }]
+});
